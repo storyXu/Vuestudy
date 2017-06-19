a39ff6646198473ecd277a859563ba7955cfeef0
xunan <xunan@pxene.com>
2017-06-19 19:05:40 +0800

    first commit

---
+++ Desktop/vue/myproject/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="header mt40 clearfix">
+      <!-- 使用 router-link 组件来导航. -->
+      <!-- 通过传入 `to` 属性指定链接. -->
+      <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+      <router-link to="/" v-bind:class="{active: isActive}" v-on:click="doSomething">Go to Hello</router-link>
+      <router-link to="/about" v-bind:class="{active: !isActive}" v-on:click="doSomething">Go to About</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'app'
+};
+const app = new Vue({
+  el: '#app',
+  data: {
+    isActive: true
+  },
+  methods: {
+    doSomething: function(){
+      this.isActive = !this.isActive;
+    }
+  }
+});
+</script>
+
+<style>
+@import "./css/index.css";
+</style>
---
+++ Desktop/vue/myproject/src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      msg: 'About Vue'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
